2e9ea9f8f51267446859c28d2801fa107eb4d0ca
thedrowned925 <chatgpticib1@gmail.com>
2026-08-23 19:50:20 +0300

    chore(android): bump seekable archive release to v0.2.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.thedrowned925.ytclone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.1.9"
+        versionCode = 11
+        versionName = "0.2.0"
     }
 
     buildFeatures {
