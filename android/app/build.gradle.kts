51f549008d8f5dce3a2fe05f1619b5488af8e49f
thedrowned925 <chatgpticib1@gmail.com>
2026-08-23 14:26:50 +0300

    chore(android): bump test version to 0.1.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.thedrowned925.ytclone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     buildFeatures {
