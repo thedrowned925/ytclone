a23dfc163f411a96127020701158d6e79f016b2a
thedrowned925 <chatgpticib1@gmail.com>
2026-08-23 05:18:11 +0300

    Initialize maintained Android yt-dlp runtime

--- android/app/src/main/java/com/thedrowned925/ytclone/YTCloneApplication.kt
+++ android/app/src/main/java/com/thedrowned925/ytclone/YTCloneApplication.kt
@@ -3,8 +3,6 @@ package com.thedrowned925.ytclone
 import android.app.Application
 import android.util.Log
 import com.yausername.youtubedl_android.YoutubeDL
-import com.yausername.youtubedl_android.aria2c.Aria2c
-import com.yausername.youtubedl_android.ffmpeg.FFmpeg
 
 class YTCloneApplication : Application() {
     override fun onCreate() {
@@ -12,12 +10,6 @@ class YTCloneApplication : Application() {
 
         runCatching { YoutubeDL.getInstance().init(this) }
             .onFailure { Log.e(TAG, "yt-dlp initialization failed", it) }
-
-        runCatching { FFmpeg.getInstance().init(this) }
-            .onFailure { Log.e(TAG, "FFmpeg initialization failed", it) }
-
-        runCatching { Aria2c.getInstance().init(this) }
-            .onFailure { Log.e(TAG, "aria2 initialization failed", it) }
     }
 
     companion object {
