33308da43c886be35591d6a507da3a3e20198f57
thedrowned925 <chatgpticib1@gmail.com>
2026-08-23 18:42:05 +0300

    chore(android): bump version to 0.1.8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.thedrowned925.ytclone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.1.7"
+        versionCode = 9
+        versionName = "0.1.8"
     }
 
     buildFeatures {
